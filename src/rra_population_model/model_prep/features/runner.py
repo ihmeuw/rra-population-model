e8dd0933ceea64930322d08ae021f04cfb9d3fc6
Ryan Barber <rmbarber@uw.edu>
2025-11-16 17:14:27 -0800

    keep v7.1 for 100m as well

--- src/rra_population_model/model_prep/features/runner.py
+++ src/rra_population_model/model_prep/features/runner.py
@@ -24,6 +24,7 @@ BUILT_VERSIONS = {
     ],
     '100': [
         pmc.BUILT_VERSIONS["ghsl_r2023a"],
+        pmc.BUILT_VERSIONS["microsoft_v7_1"],
         pmc.BUILT_VERSIONS["microsoft_v8"],
     ],
 }
